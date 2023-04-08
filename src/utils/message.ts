01895b27420d6cee9c4ebb48b427e5e4e52d88ec
Igor Kuratov <kuratov.ia@dclouds.ru>
2023-04-08 22:43:47 +0700

    change notifications

--- src/utils/message.ts
+++ src/utils/message.ts
@@ -1,22 +1,28 @@
-import { ElMessage } from 'element-plus'
+import { ElMessage, ElNotification } from 'element-plus'
 
 export const showErrorMessage = (message: string): void => {
-    ElMessage({
+    ElNotification({
+        title: 'Ошибка',
         message,
         type: 'error',
+        position: 'bottom-right',
     })
 }
 
 export const showSuccessMessage = (message: string): void => {
-    ElMessage({
+    ElNotification({
+        title: 'Успех',
         message,
         type: 'success',
+        position: 'bottom-right',
     })
 }
 
 export const showWarningMessage = (message: string): void => {
-    ElMessage({
+    ElNotification({
+        title: 'Предупреждение',
         message,
         type: 'warning',
+        position: 'bottom-right',
     })
 }
